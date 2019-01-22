6ce1c9aa8f824a2823471695c625e9f8c84c8cdd
lzy <1362699077@qq.com>
2019-01-22 15:38:25 +0800

    3.http请求界面

---
+++ app/src/main/java/com/example/lzy/review/ReqHttpActivity.java
@@ -0,0 +1,13 @@
+package com.example.lzy.review;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReqHttpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_req_http);
+    }
+}
