6e45349c6487e2f156c0edee93a7d1afbe0af42d
lzy <1362699077@qq.com>
2019-01-22 18:10:27 +0800

    5.dev测试3

---
+++ app/src/main/java/com/example/lzy/review/Test3Activity3.java
@@ -0,0 +1,13 @@
+package com.example.lzy.review;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Test3Activity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test3);
+    }
+}
